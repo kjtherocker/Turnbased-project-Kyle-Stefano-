6ce6f0074ba3e2f7b7fd1b16b710d2fdd744506a
kjtherocker <kylejohnsonthegod@gmail.com>
2018-06-20 21:06:48 -0400

    Initial commit

---
+++ Assets/Scripts/CombatScripts/Script_BattleManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Script_BattleManager : MonoBehaviour
+ {
+
+	
+
+
+void Start () 
+	{
+
+	
+	
+	}
+	
+	
+	
+void Update () 
+	{
+		
+	
+	
+	}
+
+}
