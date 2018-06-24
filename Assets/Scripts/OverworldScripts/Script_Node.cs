feb06a59566cca3ef7ffc5d1742f0894a6dab6a0
kjtherocker <kylejohnsonthegod@gmail.com>
2018-06-24 09:26:28 -0400

    Overworld Basic form is complete
    There is no random generation yet but the nodes work in this test form.

--- Assets/Scripts/OverworldScripts/Script_Node.cs
+++ Assets/Scripts/OverworldScripts/Script_Node.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Script_Node : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+
+    public Script_Node NodeUp;
+    public Script_Node NodeDown;
+    public Script_Node NodeLeft;
+    public Script_Node NodeRight;
+
+    public bool NodeEncounter;
+
+
+    void Start ()
+    {
+       
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
--- Assets/Scripts/OverworldScripts/Script_Player.cs
+++ Assets/Scripts/OverworldScripts/Script_Player.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Script_Player : MonoBehaviour {
 
     // Use this for initialization
 
-    GameObject Node1;
+    public Script_Node Node_MovingTo;
+    public Script_Node Node_PlayerIsOn;
+    public float Player_Speed = 40;
+    public bool Player_Movment = false;
+    public float Player_Speed_Delta;
 
-
-
-
-	void Start ()
+    void Start ()
     {
 		
 	}
@@ -19,6 +21,70 @@ public class Script_Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        Player_Speed_Delta = Player_Speed * Time.deltaTime;
+        //  transform.Translate(Node1.transform.localPosition );
+
+
+       
+        if (transform.position == Node_MovingTo.transform.position)
+        {
+            Player_Movment = false;
+            Node_PlayerIsOn = Node_MovingTo;
+        }
+
+        if (Node_PlayerIsOn.NodeEncounter == true)
+        {
+            SceneManager.LoadScene(3);
+        }
+        
+
+        if (Player_Movment == true)
+        {
+            OverworldMovement();
+        }
+
+        if (Player_Movment == false)
+        {
+            if (Input.GetKey("up"))
+            {
+                if (Node_PlayerIsOn.GetComponent<Script_Node>().NodeUp != null)
+                {
+                    Player_Movment = true;
+                    Node_MovingTo = Node_PlayerIsOn.GetComponent<Script_Node>().NodeUp;
+                }
+            }
+            if (Input.GetKey("down"))
+            {
+                if (Node_PlayerIsOn.GetComponent<Script_Node>().NodeDown != null)
+                {
+                    Player_Movment = true;
+                    Node_MovingTo = Node_PlayerIsOn.GetComponent<Script_Node>().NodeDown;
+                }
+            }
+            if (Input.GetKey("left"))
+            {
+                if (Node_PlayerIsOn.GetComponent<Script_Node>().NodeLeft != null)
+                {
+                    Player_Movment = true;
+                    Node_MovingTo = Node_PlayerIsOn.GetComponent<Script_Node>().NodeLeft;
+                }
+            }
+            if (Input.GetKey("right"))
+            {
+                if (Node_PlayerIsOn.GetComponent<Script_Node>().NodeRight != null)
+                {
+                    Player_Movment = true;
+                    Node_MovingTo = Node_PlayerIsOn.GetComponent<Script_Node>().NodeRight;
+                }
+            }
+
+        }
+    }
+
+    public void OverworldMovement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Node_MovingTo.transform.position, Player_Speed_Delta);
+    }
+
+
 }
