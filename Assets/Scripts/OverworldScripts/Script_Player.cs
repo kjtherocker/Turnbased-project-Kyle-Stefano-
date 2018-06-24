ff3612c83d46ee0214e2db4c5769d9b670d0628d
kjtherocker <kylejohnsonthegod@gmail.com>
2018-06-24 04:59:11 -0400

    Update so I can pull

---
+++ Assets/Scripts/OverworldScripts/Script_Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Player : MonoBehaviour {
+
+    // Use this for initialization
+
+    GameObject Node1;
+
+
+
+
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
